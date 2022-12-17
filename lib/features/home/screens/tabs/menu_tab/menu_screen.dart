3d4dbe647e5ce74b85effa57f75105d96c207f30
fsdakshay <akshaymehare2300@gmail.com>
2022-12-17 19:09:35 +0530

    menu bar page

--- lib/features/home/screens/tabs/menu_tab/menu_screen.dart
+++ lib/features/home/screens/tabs/menu_tab/menu_screen.dart
@@ -1,162 +1,190 @@
 import 'package:flutter/material.dart';
 
 
-class MyForm extends StatefulWidget {
+class MenuItems extends StatefulWidget {
+  const MenuItems({Key? key}) : super(key: key);
+
   @override
-  _MyFormState createState() => _MyFormState();
+  State<MenuItems> createState() => _MenuItemsState();
 }
 
-class _MyFormState extends State<MyForm> {
-  final _formKey = GlobalKey<FormState>();
-  late TextEditingController _nameController;
-  static List<String> friendsList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _nameController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    super.dispose();
-  }
-
+class _MenuItemsState extends State<MenuItems> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(title: Text('Dynamic TextFormFields'),),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // name textfield
-              Padding(
-                padding: const EdgeInsets.only(right: 32.0),
-                child: TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                      hintText: 'Enter your name'
+    return Container(
+       padding: const EdgeInsets.all(18.0),
+      //r=20
+      //c FF699C 35 25w
+      //t 14 55433C
+      color: const Color(0xFFF1EDF2),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
+
+                    shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.flag_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Leads",style: TextStyle(
+                            color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
                   ),
-                  // validator: (v){
-                  //   if(v?.trim().isEmpty) return 'Please enter something';
-                  //   return null;
-                  // },
                 ),
               ),
-              SizedBox(height: 20,),
-              Text('Add Friends', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),),
-              ..._getFriends(),
-              SizedBox(height: 40,),
-              ElevatedButton(
-                onPressed: (){
-                  // if(_formKey.currentState.validate()){
-                  //   _formKey.currentState.save();
-                  // }
-                },
-                child: Text('Submit'),
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
 
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.location_city_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Site Visits",style: TextStyle(
+                              color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
-
             ],
           ),
-        ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
+
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.home_work_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Flat Reservation",style: TextStyle(
+                              color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
+
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.inventory_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Inventory",style: TextStyle(
+                              color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
+
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.spatial_tracking_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Sales Tracker",style: TextStyle(
+                              color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Card(
+
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+
+                    child: Center(
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.restore_page_outlined,color: Color(0xFFFF699C),size: 35,),
+                          Text("Blockings",style: TextStyle(
+                              color: Color(0xFF55433C),fontSize: 16,fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-
     );
+
   }
-
-  /// get firends text-fields
-  List<Widget> _getFriends(){
-    List<Widget> friendsTextFields = [];
-    for(int i=0; i<friendsList.length; i++){
-      friendsTextFields.add(
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              children: [
-                Expanded(child: FriendTextFields(i)),
-                SizedBox(width: 16,),
-                // we need add button at last friends row
-                _addRemoveButton(i == friendsList.length-1, i),
-              ],
-            ),
-          )
-      );
-    }
-    return friendsTextFields;
-  }
-
-  /// add / remove button
-  Widget _addRemoveButton(bool add, int index){
-    return InkWell(
-      onTap: (){
-        if(add){
-          // add new text-fields at the top of all friends textfields
-          friendsList.insert(0, "");
-        }
-        else friendsList.removeAt(index);
-        setState((){});
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          color: (add) ? Colors.green : Colors.red,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Icon((add) ? Icons.add : Icons.remove, color: Colors.black,),
-      ),
-    );
-  }
-
-
 }
 
-class FriendTextFields extends StatefulWidget {
-  final int index;
-  FriendTextFields(this.index);
-  @override
-  _FriendTextFieldsState createState() => _FriendTextFieldsState();
-}
-
-class _FriendTextFieldsState extends State<FriendTextFields> {
-  late TextEditingController _nameController;
-
-  @override
-  void initState() {
-    super.initState();
-    _nameController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      _nameController.text = _MyFormState.friendsList[widget.index] ?? '';
-    });
-
-    return TextFormField(
-      controller: _nameController,
-      onChanged: (v) => _MyFormState.friendsList[widget.index] = v,
-      decoration: InputDecoration(
-          hintText: 'Enter your friend\'s name'
-      ),
-      validator: (v){
-        // if(v?.trim().isEmpty) return 'Please enter something';
-        // return null;
-      },
-    );
-  }
-}
