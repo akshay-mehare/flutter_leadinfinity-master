436cbc43694bfaddd34442df700f1124e77cb4fe
fsdakshay <akshaymehare2300@gmail.com>
2022-12-19 12:33:28 +0530

    some changesw

--- lib/features/home/screens/tabs/call_tab/call_screen.dart
+++ lib/features/home/screens/tabs/call_tab/call_screen.dart
@@ -1,72 +1,15 @@
 import 'package:flutter/material.dart';
 
-
-class MyHomePagee extends StatefulWidget {
-  const MyHomePagee({Key? key}) : super(key: key);
+class Contact extends StatefulWidget {
+  const Contact({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePagee> createState() => _MyHomePageeState();
+  State<Contact> createState() => _ContactState();
 }
 
-class _MyHomePageeState extends State<MyHomePagee> {
-  bool _displayNewTextField = false;
-  TextEditingController newTextFieldController = TextEditingController();
-  TextEditingController userNameController = TextEditingController();
-
+class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Form(
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: const Text("Reset Password"),
-              ),
-              const Center(
-                child: Expanded(
-                  child: Text(
-                    "Enter your username below to recieve password reset instruction",
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: userNameController,
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  labelText: "User Name",
-                ),
-              ),
-              Visibility(
-                visible: _displayNewTextField,
-                child: TextFormField(
-                  controller: newTextFieldController,
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    labelText: "User Name",
-                  ),
-                ),
-              ),
-              Container(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      _displayNewTextField = true;
-                    });
-                  },
-                  child: const Text("Submit"),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
